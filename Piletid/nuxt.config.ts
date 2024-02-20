e4577400b6f3bde7261ae61accc513efac350fdb
Kristofer P. <75545969+kristoferkp@users.noreply.github.com>
2024-02-20 22:25:56 +0000

    welp

--- Piletid/nuxt.config.ts
+++ Piletid/nuxt.config.ts
@@ -10,5 +10,20 @@ export default defineNuxtConfig({
     }
   },
 
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: ["@nuxtjs/tailwindcss", 'nuxt-vuefire'],
+
+  vuefire: {
+    config: {
+      apiKey: "AIzaSyBQv7Bd17jkpky5h5xj4nmc9wirmtKzbf0",
+      authDomain: "piletid-b625c.firebaseapp.com",
+      projectId: "piletid-b625c",
+      storageBucket: "piletid-b625c.appspot.com",
+      messagingSenderId: "190813899233",
+      appId: "1:190813899233:web:f6d49ee6896a36de992aa0",
+      measurementId: "G-FCJXKM3DP6"
+    },
+    auth: {
+      enabled: false
+    },
+  },
 })
