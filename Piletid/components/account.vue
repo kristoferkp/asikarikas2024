56cd67a87cacb2f7b359599a4cdb6d16db96a5d5
Kristofer P. <75545969+kristoferkp@users.noreply.github.com>
2024-02-26 20:44:24 +0000

    some stuff, some doesnt work yet

--- Piletid/components/account.vue
+++ Piletid/components/account.vue
@@ -133,18 +133,19 @@
 		</div>
 
 		<div class="p-5">
-			<input
+			<button
 				type="submit"
 				class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-				:value="laeb ? 'Laeb ...' : 'Uuenda'"
-				:disabled="laeb" />
+				:value="loading ? 'Laeb ...' : 'Uuenda'"
+				:disabled="loading">Uuenda
+			</button>
 		</div>
 
 		<div class="p-5">
 			<button
 				type="button"
 				@click="signOut"
-				:disabled="laeb"
+				:disabled="loading"
 				class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
 				Logi välja
 			</button>
---
+++ Piletid/components/purchases.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="purchase in data" :key="purchase.purchase_id">
+                {{ purchase.purchase_id }} - {{ purchase.tickt_id }} - {{ purchase.user_id }} - {{ purchase.purchase_date }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+	const supabase = useSupabaseClient();
+
+	const { data } = await supabase
+        .from('purchases')
+        .select()
+</script>
